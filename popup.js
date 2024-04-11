080ec69f7443bb2e10c235651c1de3e9f73a5e33
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-04-11 17:29:27 +0530

    solved-the-Magnifire-error

--- popup.js
+++ popup.js
@@ -1,12 +1,18 @@
 const btn = document.querySelector(".changeColorBtn");
 btn.addEventListener("click",async()=>{
-    let [tab] = await chrome.tabs.query({"active": true,"currentWindow":true});
+    let [tab] = await chrome.tabs.query({active: true,currentWindow:true});
     chrome.scripting.executeScript({
         target:{ tabId: tab.id },
-        function: pickColor,
+        function: pickColor(),
     })
 })
 
 async function pickColor(){
-   
+   try{
+    const eyeDropper = new EyeDropper();
+    const selectedColor = await eyeDropper.open()
+   }catch(err){
+    console.log(err)
+   }
 }
+
