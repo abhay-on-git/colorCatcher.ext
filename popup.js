20bf7706bd3b6812b556b38344734d93dd678103
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-04-14 17:33:30 +0530

    added-picked-color-history-feature

--- popup.js
+++ popup.js
@@ -1,18 +1,43 @@
 const btn = document.querySelector(".changeColorBtn");
-btn.addEventListener("click",async()=>{
-    let [tab] = await chrome.tabs.query({active: true,currentWindow:true});
-    chrome.scripting.executeScript({
-        target:{ tabId: tab.id },
-        function: pickColor(),
-    })
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+
+window.addEventListener('load',()=>{
+  const lastColor = localStorage.getItem("lastColor")
+  if(lastColor){
+    colorGrid.style.backgroundColor = lastColor;
+    colorValue.textContent = lastColor;
+  }
 })
 
-async function pickColor(){
-   try{
+btn.addEventListener("click", async () => {
+  try {
+    const color = await colorPicker();
+    localStorage.setItem("lastColor", color);
+    copyToClipboard(color);
+    colorGrid.style.backgroundColor = color;
+    colorValue.textContent = color;
+  } catch (err) {
+    console.error(err);
+    colorValue.textContent = "No access to the eye dropper API";
+  }
+});
+
+
+async function colorPicker() {
+  console.log("inside colorPicker");
+  try {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     const eyeDropper = new EyeDropper();
-    const selectedColor = await eyeDropper.open()
-   }catch(err){
-    console.log(err)
-   }
+    const { sRGBHex } = await eyeDropper.open();
+    console.log("After await", sRGBHex);
+    return sRGBHex;
+  } catch (err) {
+    console.error(err);
+    throw new Error("No access to the eye dropper API");
+  }
 }
 
+async function copyToClipboard(text) {
+ await navigator.clipboard.writeText(text);
+}
